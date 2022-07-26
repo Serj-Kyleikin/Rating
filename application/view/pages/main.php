d18fb2299ea0626693935dd0ea10f4144e8cdf60
Serj <93035922+Serj-Kyleikin@users.noreply.github.com>
2022-07-26 20:07:52 +0300

    Update main.php

--- application/view/pages/main.php
+++ application/view/pages/main.php
@@ -6,24 +6,28 @@
 ?>
 
 <div class="articles">
-    <?php for($i=0; $i<$count; $i++): ?>
-        <?php $article = $page[0]['static'][$i]; ?>
-        <?php $rating = $page[0]['dynamic']['content'][$i]; ?>
+
+    <?php for($i=0; $i<$count; $i++): 
+
+        $article = $page[0]['static'][$i];
+        $rating = $page[0]['dynamic']['content'][$i]; 
+    ?>
+
         <div class="article" data-article="<?= $article['id']; ?>">
             <?= $article['text']; ?>
             <div class="rating">
-            <div class="ratingDefault"></div>
-            <div class="ratingResult" style="width: <?php echo 160 / 5 * $rating['rating'] / 100 . 'px'; ?>"></div>
-            <div class="ratingVote">
-                <p data-vote="1"></p>
-                <p data-vote="2"></p>
-                <p data-vote="3"></p>
-                <p data-vote="4"></p>
-                <p data-vote="5"></p>
+                <div class="ratingDefault"></div>
+                <div class="ratingResult" style="width: <?php echo 160 / 5 * $rating['rating'] / 100 . 'px'; ?>"></div>
+                <div class="ratingVote">
+                    <p data-vote="1"></p>
+                    <p data-vote="2"></p>
+                    <p data-vote="3"></p>
+                    <p data-vote="4"></p>
+                    <p data-vote="5"></p>
+                </div>
+                <div class="voters">Проголосовало: <? echo ($rating['voters']) ? $rating['voters'] : 0; ?></div>
+                <?php if(isset($rating['vote']) and $rating['vote'] != null): echo "<p class='user_vote'>Ваша оценка: " . $rating['vote'] . '</p>'; endif; ?>
             </div>
-            <div class="voters">Проголосовало: <? echo ($rating['voters']) ? $rating['voters'] : 0; ?></div>
-            <?php if(isset($rating['vote']) and $rating['vote'] != null): echo "<p class='user_vote'>Ваша оценка: " . $rating['vote'] . '</p>'; endif; ?>
-        </div>
         </div>
     <?php endfor; ?>
 </div>
